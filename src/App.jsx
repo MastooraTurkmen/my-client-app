dbc828dac425af8eab6cbd9d9a7fa75c38b01599
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-17 21:32:30 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+   <h2>Hello world</h2>
+ )
+}
+
+export default App
