59d3c7d2f6196efae43096fdc1531ad3752e0f52
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-18 20:24:45 +0430

    pages set up

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+export { default as DashboardLayout } from './DashboardLayout';
+export { default as Admin } from './Admin';
+export { default as AddJob } from './AddJob';
+export { default as DeleteJob } from './DeleteJob';
+export { default as AllJob } from './AllJob';
+export { default as EditJob } from './EditJob';
+export { default as Profile } from './Profile';
+export { default as Landing } from './Landing';
+export { default as Register } from './Register';
+export { default as Login } from './Login';
+export { default as HomeLayout } from './HomeLayout';
+export { default as Stats } from './Stats';
+export { default as Error } from './Error';
