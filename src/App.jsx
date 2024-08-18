ce960a69d5d8cead4e3159d7fea4542411e4081a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-18 20:24:06 +0430

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import {
+  AddJob, Admin, AllJob, DashboardLayout, Profile, Register,
+  DeleteJob, EditJob, Error, HomeLayout, Landing, Login, Stats
+} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ]
+  },
+   
+])
+
 
 function App() {
   return (
-   <h2>Hello world</h2>
+    <RouterProvider router={router} />
  )
 }
 
