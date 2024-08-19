d921be3b0d60f3baa5c51d2555693309c6e7e4b7
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-19 10:12:14 +0430

    register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
-import { Logo } from "../components";
+import { Logo, FromRow } from "../components";
 
 const Register = () => {
   return (
@@ -8,10 +8,11 @@ const Register = () => {
       <form className="form">
         <Logo />
         <h4>Register</h4>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">name</label>
-          <input type="text" id="name" name="name" className="form-input"  />
-        </div>
+        <FromRow type="text" name="name" placeholder="john" />
+        <FromRow type="text" name="lastName" labelText='Last Name' placeholder="smith" />
+        <FromRow type="text" name="location" placeholder="earth" />
+        <FromRow type="email" name="email" placeholder="john@gmail.com" />
+        <FromRow type="password" name="password" placeholder="secret123" />
         <button type="submit" className="btn btn-block">submit</button>
         <p>
           Already a member? <Link to='login'>login</Link>
