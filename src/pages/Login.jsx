a37c214991ac29307d77497af29da19cd4850ea1
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-24 21:22:10 +0430

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom"
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { Logo, FromRow } from "../components"
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FromRow type="email" name="email" placeholder="john@gmail.com" />
+        <FromRow type="password" name="password" placeholder="secrect123" />
+        <button type="submit" className="btn btn-block">Submit</button>
+        <button type="submit" className="btn btn-block">explore the app</button>
+        <p>
+          Not a member yet? <Link to='/register' className="member-btn">Register</Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
