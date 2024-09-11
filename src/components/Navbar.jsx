ed8bf81a77a589b3c34a1982c394837099e7af91
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-11 15:32:13 +0430

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+import { userDashboardContext } from "../pages/DashboardLayout";
+
+const Navbar = () => {
+  const { toggleSidebar } = userDashboardContext();
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
