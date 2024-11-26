51e7fa501eece312107423f41e316c3d965d83e4
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-26 20:03:38 +0430

    status CODE

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,19 +1,14 @@
+import { StatusCodes } from 'http-status-codes';
 import Job from '../models/JobModel.js';
-import 'express-async-errors'
 
 export const getAllJobs = async (req, res) => {
     const jobs = await Job.find(req.body)
-    res.status(200).json({ jobs })
+    res.status(StatusCodes.OK).json({ jobs })
 }
 
 export const createJob = async (req, res) => {
-    try {
-        const { company, position } = req.body
-        const job = await Job.create({ company, position })
-        res.status(201).json({ job })
-    } catch (error) {
-        res.status(500).json({ msg: 'server error' })
-    }
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 export const getSingleJob = async (req, res) => {
@@ -22,7 +17,7 @@ export const getSingleJob = async (req, res) => {
     if (!job) {
         return res.status(404).json({ msg: `no job with id ${id}` })
     }
-    res.status(200).json({ job })
+    res.status(StatusCodes.OK).json({ job })
 }
 
 export const updateJob = async (req, res) => {
@@ -33,7 +28,7 @@ export const updateJob = async (req, res) => {
     if (!updatedJob) {
         return res.status(404).json({ msg: `no job with id ${id}` })
     }
-    res.status(200).json({ msg: 'job modified', job: updatedJob })
+    res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob })
 }
 
 export const deleteJob = async (req, res) => {
@@ -44,5 +39,5 @@ export const deleteJob = async (req, res) => {
         return res.status(404).json({ msg: `no job with id ${id}` })
     }
 
-    res.status(200).json({ msg: 'job deleted', job: removedJob })
+    res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob })
 }
