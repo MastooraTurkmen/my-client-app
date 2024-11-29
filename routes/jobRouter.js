432b22b268a819129b2c9e73e36dfd435eab4595
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-29 14:21:49 +0430

    job router

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 const router = Router()
 import { getAllJobs, getSingleJob, updateJob, createJob, deleteJob } from '../controllers/jobController.js'
-import { validateJobInput } from '../middleware/validationMiddleware.js'
+import { validateJobInput, validateIdParams } from '../middleware/validationMiddleware.js'
 
 router.route('/').get(getAllJobs).post(validateJobInput, createJob)
-router.route('/:id').get(getSingleJob).patch(validateJobInput, updateJob).delete(deleteJob)
+router.route('/:id').get(validateIdParams, getSingleJob).patch(validateJobInput, updateJob).delete(validateIdParams, deleteJob)
 
 export default router;
