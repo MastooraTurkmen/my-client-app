c49340a44cf14e2895b0cd94fa16f9f512824cbe
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-29 14:21:55 +0430

    constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,19 @@
 export const JOB_STATUS = {
     PENDING: 'pending',
+    "pending": 'pending',
     INTERVIEW: 'interview',
-    DECLINED: 'declined'
+    "interview": 'interview',
+    DECLINED: 'declined',
+    "declined": 'declined'
 }
 
 export const JOB_TYPE = {
     FULL_TIME: 'full-time',
+    "full-time": 'full-time',
     PART_TIME: 'part-time',
-    INTERNSHIP: 'internship'
+    "part-time": 'part-time',
+    INTERNSHIP: 'internship',
+    "internship": 'internship'
 }
 
 export const JOB_SORT_BY = {
