ab4bf06c3ccf03d97d70e38631758fc5a72017d5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-29 14:20:47 +0430

    auth router

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import mongoose from 'mongoose';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
+import authRouter from './routes/authRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
@@ -23,6 +24,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/jobs', jobRouter)
+app.use('/api/v1/auth', authRouter)
 
 app.use('*', (req, res) => {
     res.status(404).json({ msg: "not found" })
