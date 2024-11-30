ae652507e0ca95fe49a6e2e5da5d6358830dbd28
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-30 18:20:00 +0430

    job controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,11 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import Job from '../models/JobModel.js';
 
 export const getAllJobs = async (req, res) => {
-    const jobs = await Job.find(req.body)
+    const jobs = await Job.find({ createdBy: req.user.userId })
     res.status(StatusCodes.OK).json({ jobs })
 }
 
 export const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({ job })
 }
