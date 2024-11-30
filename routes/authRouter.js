cd50416846fd241e67ca78d79c784947df5fae99
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-30 18:20:36 +0430

    auth router

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { login, register } from "../controllers/authController.js";
-import { validateRegisterInput } from "../middleware/validationMiddleware.js";
+import { validateRegisterInput, validateLoginInput } from "../middleware/validationMiddleware.js";
 
 const router = Router()
 
 router.post('/register', validateRegisterInput, register)
-router.post('/login', login)
+router.post('/login', validateLoginInput, login)
 
 export default router;
