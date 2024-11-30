55764a8813865baaa8c8c1e96cf13df04146221d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-30 18:20:23 +0430

    validate login input

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -45,4 +45,9 @@ export const validateRegisterInput = withValidationErrors([
         }),
     body('password').notEmpty().withMessage('password is required').isLength({ min: 8 }).withMessage('password must be at least 8 character'),
     body('location').notEmpty().withMessage('location is required'),
+])
+
+export const validateLoginInput = withValidationErrors([
+    body('email').notEmpty().withMessage('email is required').isEmail().withMessage('invalid email format'),
+    body('password').notEmpty().withMessage('password is required') 
 ])  
