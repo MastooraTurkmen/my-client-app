acb1adab9fca29cf07a0099eb192079267bfede1
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-30 18:19:47 +0430

    cookie parser

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
@@ -12,18 +13,20 @@ import authRouter from './routes/authRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use(cookieParser())
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.use('/api/v1/jobs', jobRouter)
+app.use('/api/v1/jobs', authenticateUser, jobRouter)
 app.use('/api/v1/auth', authRouter)
 
 app.use('*', (req, res) => {
