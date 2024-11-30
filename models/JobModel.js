a7ed92bf9dd3df8bf66aa8f29f80a6ccdd5c3296
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-30 18:20:31 +0430

    job model

--- models/JobModel.js
+++ models/JobModel.js
@@ -22,6 +22,10 @@ const JobSchema = new mongoose.Schema({
     jobLocation: {
         type: String,
         default: "my city"
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
     }
 }, { timestamps: true })
 
