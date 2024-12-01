0e469c038b9dedae007f858ff9fc7f7ffd03462f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-01 19:34:39 +0430

    server js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cookieParser from 'cookie-parser';
 // routers
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
@@ -26,7 +27,12 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/api/v1/test', (req, res) => {
+    res.json({msg: 'test route'})
+})
+
 app.use('/api/v1/jobs', authenticateUser, jobRouter)
+app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
 app.use('*', (req, res) => {
