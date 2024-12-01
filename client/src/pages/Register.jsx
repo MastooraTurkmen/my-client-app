7e149aaf3d430d801fba8da712a64248911fbf0e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-01 19:35:14 +0430

    register setup completed

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,25 +1,55 @@
-import { Link } from "react-router-dom";
-import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { Link, redirect, useNavigation, Form } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo, FromRow } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  try {
+    await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
+    return redirect("/login");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Register = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
         <FromRow type="text" name="name" placeholder="john" />
-        <FromRow type="text" name="lastName" labelText='Last Name' placeholder="smith" />
+        <FromRow
+          type="text"
+          name="lastName"
+          labelText="Last Name"
+          placeholder="smith"
+        />
         <FromRow type="text" name="location" placeholder="earth" />
         <FromRow type="email" name="email" placeholder="john@gmail.com" />
         <FromRow type="password" name="password" placeholder="secret123" />
-        <button type="submit" className="btn btn-block">submit</button>
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
+        </button>
         <p>
-          Already a member? <Link to='/login' className="member-btn">Login</Link>
+          Already a member?{" "}
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
