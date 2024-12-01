0943621f91e239da15f47e38583685aa27ac7e68
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-01 19:36:19 +0430

    permission

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -45,9 +45,23 @@ export const validateRegisterInput = withValidationErrors([
         }),
     body('password').notEmpty().withMessage('password is required').isLength({ min: 8 }).withMessage('password must be at least 8 character'),
     body('location').notEmpty().withMessage('location is required'),
+    body('lastName').notEmpty().withMessage('lastName is required'),
 ])
 
 export const validateLoginInput = withValidationErrors([
     body('email').notEmpty().withMessage('email is required').isEmail().withMessage('invalid email format'),
-    body('password').notEmpty().withMessage('password is required') 
-])  
+    body('password').notEmpty().withMessage('password is required')
+])
+
+
+export const validateUpdateUserInput = withValidationErrors([
+    body('name').notEmpty().withMessage('name is required'),
+    body('email').notEmpty().withMessage('email is required').isEmail().withMessage('invalid email format').custom(
+        async (email) => {
+            const user = await User.findOne({ email })
+            if (user && user._id.toString() !== req.user.userId) {
+                throw new BadRequestError('email already exists')
+            }
+        }),
+    body('location').notEmpty().withMessage('location is required'),
+])
