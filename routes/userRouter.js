804e7f7d19b0eb2868068fd62281226e98d1e8b2
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-01 19:36:35 +0430

    user router

---
+++ routes/userRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
+import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
+import { authorizedPermissions } from "../middleware/authMiddleware.js";
+const router = Router()
+
+router.get('/current-user', getCurrentUser)
+router.get('/admin/app-stats', [authorizedPermissions('admin'), getApplicationStats])
+router.patch('/update-user', validateUpdateUserInput, updateUser)
+
+export default router;
