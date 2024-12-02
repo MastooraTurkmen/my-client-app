36a50e7118b3b34796838fe3d271648fed5f8534
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-02 17:10:46 +0430

    constants

--- utils/constants.js
+++ utils/constants.js
@@ -15,7 +15,17 @@ export const JOB_TYPE = {
     INTERNSHIP: 'internship',
     "internship": 'internship'
 }
+export const JOB_STATUS1 = {
+    PENDING: 'pending',
+    INTERVIEW: 'interview',
+    DECLINED: 'declined',
+}
 
+export const JOB_TYPE1 = {
+    FULL_TIME: 'full-time',
+    PART_TIME: 'part-time',
+    INTERNSHIP: 'internship',
+}
 export const JOB_SORT_BY = {
     NEWEST_FIRST: 'newest',
     OLDEST_FIRST: 'oldest',
