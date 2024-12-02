d29e0c79e6c07c43ca02aede60fb87de128ba3e4
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-02 17:09:30 +0430

    form row

--- client/src/components/FromRow.jsx
+++ client/src/components/FromRow.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const FromRow = ({type, name, labelText, placeholder}) => {
+const FromRow = ({ type, name, labelText, placeholder }) => {
   return (
-        <div>
-          <div className="form-row">
-            <label htmlFor={name} className="form-label">{labelText || name}</label>
-            <input placeholder={placeholder || ''} type={type} required id={name} name={name} className="form-input"  />
-            </div>
-        </div>
-  )
-}
+    <div>
+      <div className="form-row">
+        <label htmlFor={name} className="form-label">
+          {labelText || name}
+        </label>
+        <input
+          placeholder={placeholder || ""}
+          type={type}
+          required
+          id={name}
+          name={name}
+          className="form-input"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default FromRow
+export default FromRow;
