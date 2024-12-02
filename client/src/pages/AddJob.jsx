7b154e352f6a7b60e87fa2f300ef1e260be02218
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-02 17:10:11 +0430

    add job

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,64 @@
-const AddJob = () => {
-  return (
-    <div>AddJob</div>
-  )
-}
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS1, JOB_TYPE1 } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+import { FromRow, FormRowSelect } from "../components";
 
-export default AddJob
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/jobs", data);
+    toast.success("job successfully added");
+    return redirect('all-jobs');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
+
+const AddJob = () => {
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FromRow type="text" name="position" />
+          <FromRow type="text" name="company" />
+          <FromRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+          <FormRowSelect
+            labelText="job status"
+            name="jobStatus"
+            defaultValue={JOB_STATUS1.PENDING}
+            list={Object.values(JOB_STATUS1)}
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            defaultValue={JOB_TYPE1.FULL_TIME}
+            list={Object.values(JOB_TYPE1)}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting && false}
+          >
+            {isSubmitting ? "submitting.." : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default AddJob;
