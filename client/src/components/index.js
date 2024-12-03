5eb80d2e3e5d0625842f1f6f636cd6f364ae7e4f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-03 21:55:29 +0430

    name error solved

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Logo } from './Logo';
-export { default as FromRow } from './FromRow';
+export { default as FormRow } from './FormRow';
 export { default as BigSidebar } from './BigSidebar';
 export { default as SmallSidebar } from './SmallSidebar';
 export { default as Navbar } from './Navbar';
