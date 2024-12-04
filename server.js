4b9558340b38e7002332c7c0dbb026899bc62c5c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-04 21:49:42 +0430

    cloudinary set up

--- server.js
+++ server.js
@@ -11,15 +11,31 @@ import cookieParser from 'cookie-parser';
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
+import cloudinary from 'cloudinary';
+
+// public
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use(express.static(path.resolve(__dirname, './public')))
 app.use(cookieParser())
 app.use(express.json());
 
@@ -28,7 +44,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/v1/test', (req, res) => {
-    res.json({msg: 'test route'})
+    res.json({ msg: 'test route' })
 })
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter)
