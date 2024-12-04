1e324cf4ea40d6e3d25fa4e6d096d9c4d4ceded0
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-04 21:51:46 +0430

    profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,68 @@
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { useNavigation, Form } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
-export default Profile
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const file = formData.get("avatar");
+  if (file && file.size > 500000) {
+    toast.error("Image size is too large");
+    return null;
+  }
+  try {
+    await customFetch.patch("/users/update-user", formData);
+    toast.success("Profile updated successfully");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
+
+const Profile = () => {
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlFor="avatar" className="form-label">
+              Select an image file (max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              className="form-input"
+              accept="image/*"
+            />
+          </div>
+          <FormRow type="text" name="name" defaultValue={name} />
+          <FormRow type="email" name="email" defaultValue={email} />
+          <FormRow type="text" name="location" defaultValue={location} />
+          <FormRow
+            type="text"
+            name="lastName"
+            defaultValue={lastName}
+            labelText="last name"
+          />
+          <button
+            className="btn btn-block form-btn"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting" : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default Profile;
