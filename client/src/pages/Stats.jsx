57e08a4606816d4c99ebb4d772672db72637e1aa
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-07 06:47:52 +0430

    stats

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,7 +1,26 @@
-const Stats = () => {
-  return (
-    <div>Stats</div>
-  )
-}
+import { ChartsContainer, StatsContainer } from "../components";
+import customFetch from "../utils/customFetch";
+import { useLoaderData } from "react-router-dom";
 
-export default Stats
+export const loader = async () => {
+  try {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const Stats = () => {
+  const { defaultStats, monthlyApplication } = useLoaderData();
+  return (
+    <>
+      <StatsContainer defaultStats={defaultStats} />
+      {monthlyApplication?.length > 1 && (
+        <ChartsContainer data={monthlyApplication} />
+      )}
+    </>
+  );
+};
+
+export default Stats;
