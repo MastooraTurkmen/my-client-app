24a0fd2a7f88f69e767c5b5786d48a127225ceb3
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-07 06:47:12 +0430

    bar chart

---
+++ client/src/components/BarChart.jsx
@@ -0,0 +1,24 @@
+import {
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  BarChart,
+  Bar,
+} from "recharts";
+
+const BarChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="data" />
+        <YAxis allowDecimals={false} />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarChartComponent;
