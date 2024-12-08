83510c05bec249f98aeff2ae165d95e5724d7873
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 21:04:36 +0430

    clean up

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,33 +1,30 @@
-import { StatusCodes } from "http-status-codes";
+import { StatusCodes } from 'http-status-codes';
 
 export class NotFoundError extends Error {
     constructor(message) {
-        super(message)
+        super(message);
         this.name = 'NotFoundError';
-        this.statusCode = StatusCodes.NOT_FOUND
+        this.statusCode = StatusCodes.NOT_FOUND;
     }
 }
-
 export class BadRequestError extends Error {
     constructor(message) {
-        super(message)
+        super(message);
         this.name = 'BadRequestError';
-        this.statusCode = StatusCodes.BAD_REQUEST
+        this.statusCode = StatusCodes.BAD_REQUEST;
     }
 }
-
 export class UnauthenticatedError extends Error {
     constructor(message) {
-        super(message)
+        super(message);
         this.name = 'UnauthenticatedError';
-        this.statusCode = StatusCodes.UNAUTHORIZED
+        this.statusCode = StatusCodes.UNAUTHORIZED;
     }
 }
-
-export class Unauthorized extends Error {
+export class UnauthorizedError extends Error {
     constructor(message) {
-        super(message)
-        this.name = 'Unauthorized';
-        this.statusCode = StatusCodes.FORBIDDEN
+        super(message);
+        this.name = 'UnauthorizedError';
+        this.statusCode = StatusCodes.FORBIDDEN;
     }
 }
