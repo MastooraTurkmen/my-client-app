ed2556b671bfcf406b413bfac70b455408ae4387
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:53:41 +0430

    correct the spelling

--- client/src/components/StatItem.jsx
+++ client/src/components/StatItem.jsx
@@ -1,15 +1,14 @@
 import Wrapper from "../assets/wrappers/StatItem";
 
-const StatItem = ({ color, title, count, icon, bcg }) => {
-    return (
-      <Wrapper color={color} bcg={bcg}>
-        <header>
-          <span className="count">{count}</span>
-          <span className="icon">{count}</span>
-            </header>
-            <h5 className="title">{title}</h5>
-      </Wrapper>
-    );
+const StatItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
 };
-
 export default StatItem;
