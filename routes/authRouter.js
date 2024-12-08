d1aa3a1b4d4a32fd6181c48cd07f804340679b48
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 21:05:03 +0430

    clean up

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,21 @@
-import { Router } from "express";
-import { login, logout, register } from "../controllers/authController.js";
-import { validateRegisterInput, validateLoginInput } from "../middleware/validationMiddleware.js";
+import { Router } from 'express';
+const router = Router();
+import { login, logout, register } from '../controllers/authController.js';
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from '../middleware/validationMiddleware.js';
 
-const router = Router()
+import rateLimiter from 'express-rate-limit';
 
-router.post('/register', validateRegisterInput, register)
-router.post('/login', validateLoginInput, login)
-router.post('/logout', logout)
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20,
+  message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
+});
+
+router.post('/register', apiLimiter, validateRegisterInput, register);
+router.post('/login', apiLimiter, validateLoginInput, login);
+router.get('/logout', logout);
 
 export default router;
