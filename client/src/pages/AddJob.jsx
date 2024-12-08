4fcc2a5d5f90522661185935357b062d609914a3
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:31:58 +0430

    add job

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,26 +1,30 @@
+import { FormRow, FormRowSelect, SubmitBtn } from "../components";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
 import { useOutletContext } from "react-router-dom";
-import { JOB_STATUS1, JOB_TYPE1 } from "../../../utils/constants";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
 import { Form, redirect } from "react-router-dom";
 import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
-import { FormRow, FormRowSelect, SubmitBtn } from "../components";
 
-export const action = async ({ request }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  try {
-    await customFetch.post("/jobs", data);
-    toast.success("job successfully added");
-    return redirect("/dashboard/all-jobs");
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
-  }
-};
+export const action =
+  (queryClient) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+      await customFetch.post("/jobs", data);
+      queryClient.invalidateQueries(["jobs"]);
+      toast.success("Job added successfully ");
+      return redirect("all-jobs");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return error;
+    }
+  };
 
 const AddJob = () => {
   const { user } = useOutletContext();
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -37,14 +41,14 @@ const AddJob = () => {
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
-            defaultValue={JOB_STATUS1.PENDING}
-            list={Object.values(JOB_STATUS1)}
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
           />
           <FormRowSelect
             labelText="job type"
             name="jobType"
-            defaultValue={JOB_TYPE1.FULL_TIME}
-            list={Object.values(JOB_TYPE1)}
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
           />
           <SubmitBtn formBtn />
         </div>
@@ -52,5 +56,4 @@ const AddJob = () => {
     </Wrapper>
   );
 };
-
 export default AddJob;
