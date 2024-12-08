fc010cd88c0d84e6e3e0bb9aa433fd8c56bb924d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:18 +0430

    set up

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,13 +1,17 @@
-import { toast } from "react-toastify";
-import customFetch from "../utils/customFetch";
-import { redirect } from "react-router-dom";
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+import { redirect } from 'react-router-dom';
 
-export async function action({ params }) {
-  try {
-    await customFetch(`/jobs/${params.id}`);
-    toast.success("Job deleted");
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-  }
-  return redirect("/dashboard/all-jobs");
-}
+export const action =
+  (queryClient) =>
+  async ({ params }) => {
+    try {
+      await customFetch.delete(`/jobs/${params.id}`);
+      queryClient.invalidateQueries(['jobs']);
+
+      toast.success('Job deleted successfully');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+    return redirect('/dashboard/all-jobs');
+  };
