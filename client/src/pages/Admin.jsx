6857f0322d48bff79fd4873010648c05cec38b08
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:04 +0430

    correct the spelling

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,19 +1,17 @@
-import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
-
-import { useLoaderData, redirect } from "react-router-dom";
-import customFetch from "../utils/customFetch";
-import Wrapper from "../assets/wrappers/StatsContainer";
-import { toast } from "react-toastify";
-import { StatItem } from "../components";
+import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa';
+import { useLoaderData, redirect } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import { toast } from 'react-toastify';
+import { StatItem } from '../components';
 
 export const loader = async () => {
   try {
-    const response = await customFetch.get("/users/admin/app-stats");
+    const response = await customFetch.get('/users/admin/app-stats');
     return response.data;
   } catch (error) {
-    console.log(error);
-    toast.error("You are not authorized to view this page");
-    return redirect("/dashboard");
+    toast.error('You are not authorized to view this page');
+    return redirect('/dashboard');
   }
 };
 
@@ -22,21 +20,20 @@ const Admin = () => {
   return (
     <Wrapper>
       <StatItem
-        title="current users"
+        title='current users'
         count={users}
-        color="#e9b949"
-        bcg="#fcefc7"
+        color='#e9b949'
+        bcg='#fcefc7'
         icon={<FaSuitcaseRolling />}
       />
       <StatItem
-        title="total jobs"
+        title='total jobs'
         count={jobs}
-        color="#647acb"
-        bcg="#e0e8f9"
+        color='#647acb'
+        bcg='#e0e8f9'
         icon={<FaCalendarCheck />}
       />
     </Wrapper>
   );
 };
-
 export default Admin;
