86ab0ed5c504f71ca5473a98e17eaac52a8e3226
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 21:04:39 +0430

    clean up

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,30 +1,34 @@
-import { BadRequestError, UnauthenticatedError, Unauthorized } from "../errors/customErrors.js"
-import { verifyJWT } from "../utils/tokenUtils.js"
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+  BadRequestError,
+} from '../errors/customErrors.js';
+import { verifyJWT } from '../utils/tokenUtils.js';
 
-export const authenticateUser = async (req, res, next) => {
-    const { token } = req.cookies
-    if (!token) throw new UnauthenticatedError('authentication invalid')
+export const authenticateUser = (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError('authentication invalid');
 
-    try {
-        const { userId, role } = verifyJWT(token)
-        const testUser = userId === "67519d865b464c931c3b2ceb"
-        req.user = { userId, role, testUser }
-        next()
-    } catch (error) {
-        throw new UnauthenticatedError('authentication invalid')
+  try {
+    const { userId, role } = verifyJWT(token);
+    const testUser = userId === '64b2c07ccac2efc972ab0eca';
+    req.user = { userId, role, testUser };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('authentication invalid');
+  }
+};
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError('Unauthorized to access this route');
     }
-}
-
-export const authorizedPermissions = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            throw new Unauthorized('Unauthorized to access to this route')
-        }
-        next();
-    }
-}
+    next();
+  };
+};
 
 export const checkForTestUser = (req, res, next) => {
-    if (req.user.testUser) throw new BadRequestError('Demo User. Read Only!')
-    next()
-}
+  if (req.user.testUser) throw new BadRequestError('Demo User. Read Only!');
+  next();
+};
