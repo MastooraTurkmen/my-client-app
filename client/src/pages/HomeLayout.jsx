77a689b7959f9fff7d4cef471d50f1c1e32d8271
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:28 +0430

    set up

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,11 +1,9 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from 'react-router-dom';
 const HomeLayout = () => {
   return (
-    <div>
+    <>
       <Outlet />
-    </div>
-  )
-}
-
-export default HomeLayout
+    </>
+  );
+};
+export default HomeLayout;
