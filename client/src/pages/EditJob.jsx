2695f7abd541ffb6694a6516c82bf7223daf22d1
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:22 +0430

    set up

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,61 +1,79 @@
-import { FormRow, FormRowSelect, SubmitBtn } from "../components";
-import Wrapper from "../assets/wrappers/DashboardFormPage";
-import { useLoaderData, useParams } from "react-router-dom";
-import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
-import { Form, redirect } from "react-router-dom";
-import { toast } from "react-toastify";
-import customFetch from "../utils/customFetch";
+import { FormRow, FormRowSelect, SubmitBtn } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+import { useQuery } from '@tanstack/react-query';
 
-export const loader = async ({ params }) => {
-  console.log(params);
-
-  try {
-    const { data } = await customFetch.get(`/jobs/${params.id}`);
-    return data;
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return redirect("/dashboard/all-jobs");
-  }
+const singleJobQuery = (id) => {
+  return {
+    queryKey: ['job', id],
+    queryFn: async () => {
+      const { data } = await customFetch.get(`/jobs/${id}`);
+      return data;
+    },
+  };
 };
 
-export const action = async ({ request, params }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  try {
-    await customFetch.patch(`/jobs/${params.id}`, data);
-    toast.success("Job edited successfully");
-    return redirect("/dashboard/all-jobs");
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
-  }
-};
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    try {
+      await queryClient.ensureQueryData(singleJobQuery(params.id));
+      return params.id;
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return redirect('/dashboard/all-jobs');
+    }
+  };
+export const action =
+  (queryClient) =>
+  async ({ request, params }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+      await customFetch.patch(`/jobs/${params.id}`, data);
+      queryClient.invalidateQueries(['jobs']);
+
+      toast.success('Job edited successfully');
+      return redirect('/dashboard/all-jobs');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return error;
+    }
+  };
 
 const EditJob = () => {
-  const { job } = useLoaderData();
+  const id = useLoaderData();
+
+  const {
+    data: { job },
+  } = useQuery(singleJobQuery(id));
 
   return (
     <Wrapper>
-      <Form method="post" className="form">
-        <h4 className="form-title">edit job</h4>
-        <div className="form-center">
-          <FormRow type="text" name="position" defaultValue={job.position} />
-          <FormRow type="text" name="company" defaultValue={job.company} />
+      <Form method='post' className='form'>
+        <h4 className='form-title'>edit job</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='position' defaultValue={job.position} />
+          <FormRow type='text' name='company' defaultValue={job.company} />
           <FormRow
-            type="text"
-            name="jobLocation"
-            labelText="job location"
+            type='text'
+            name='jobLocation'
+            labelText='job location'
             defaultValue={job.jobLocation}
           />
           <FormRowSelect
-            name="jobStatus"
-            labelText="job status"
+            name='jobStatus'
+            labelText='job status'
             defaultValue={job.jobStatus}
             list={Object.values(JOB_STATUS)}
           />
           <FormRowSelect
-            name="jobType"
-            labelText="job type"
+            name='jobType'
+            labelText='job type'
             defaultValue={job.jobType}
             list={Object.values(JOB_TYPE)}
           />
@@ -65,5 +83,4 @@ const EditJob = () => {
     </Wrapper>
   );
 };
-
 export default EditJob;
