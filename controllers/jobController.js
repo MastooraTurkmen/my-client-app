e5e62e432eb37ff194a1d95d2ba08ff0c573d5af
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:32:26 +0430

    job controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import day from 'dayjs';
 
 export const getAllJobs = async (req, res) => {
-    const { search, jobStatus, jobType } = req.query
+    const { search, jobStatus, jobType, sort } = req.query
     const queryObject = {
         createdBy: req.user.userId,
         jobStatus,
@@ -25,8 +25,26 @@ export const getAllJobs = async (req, res) => {
         queryObject.jobType = jobType;
     }
 
-    const jobs = await Job.find(queryObject).sort('-createdAt')
-    res.status(StatusCodes.OK).json({ jobs })
+    const sortOptions = {
+        newest: '-createdAt',
+        oldest: 'createdAt',
+        'a-z': 'position',
+        'z-a': '-position'
+    }
+
+    const sortKey = sortOptions[sort] || sortOptions.newest;
+
+
+    // setup pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    const jobs = await Job.find(queryObject).sort(sortKey).skip(skip).limit(limit)
+    const totalJobs = await Job.countDocuments(queryObject)
+    const numOfPages = Math.ceil(totalJobs / limit)
+
+    res.status(StatusCodes.OK).json({ totalJobs, numOfPages, currentPage: page, jobs })
 }
 
 export const createJob = async (req, res) => {
