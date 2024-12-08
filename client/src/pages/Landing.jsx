176e8a146a2330d57f9c20f3c485605a8ed3fff7
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:34 +0430

    set up

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,8 @@
-import styled from "styled-components"
-import main from '../assets/images/main.svg'
-import Wrapper from '../assets/wrappers/LandingPage'
-import { Link } from "react-router-dom"
-import { Logo } from "../components"
-
+import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
@@ -11,24 +10,28 @@ const Landing = () => {
       <nav>
         <Logo />
       </nav>
-      <div className="container page">
-        <div className="info">
+      <div className='container page'>
+        <div className='info'>
           <h1>
             job <span>tracking</span> app
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            In facere officiis, minus, repudiandae magni placeat
-            recusandae tempora fugiat explicabo totam perferendis
-            architecto nulla ex officia, vitae a labore accusamus dolorum!
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
           </p>
-          <Link to='/register' className="btn register-link">Register</Link>
-          <Link to='/login' className="btn login-link">Login / Demo User</Link>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn '>
+            Login / Demo User
+          </Link>
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
+        <img src={main} alt='job hunt' className='img main-img' />
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Landing;
