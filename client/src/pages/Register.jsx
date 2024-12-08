1b6a3b21b20170539cc8eb260636cbb2e2e283ba
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:43 +0430

    set up

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,45 +1,37 @@
-import { Link, redirect, useNavigation, Form } from "react-router-dom";
-import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Logo, FormRow, SubmitBtn } from "../components";
-import customFetch from "../utils/customFetch";
-import { toast } from "react-toastify";
-
+import { Form, redirect, Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo, SubmitBtn } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+
   try {
-    await customFetch.post("/auth/register", data);
-    toast.success("Registration successful");
-    // return redirect("/login");
-    return null;
+    await customFetch.post('/auth/register', data);
+    toast.success('Registration successful');
+    return redirect('/login');
   } catch (error) {
     toast.error(error?.response?.data?.msg);
+
     return error;
   }
 };
-
 const Register = () => {
-
   return (
     <Wrapper>
-      <Form method="post" className="form">
+      <Form method='post' className='form'>
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" placeholder="john" />
-        <FormRow
-          type="text"
-          name="lastName"
-          labelText="Last Name"
-          placeholder="smith"
-        />
-        <FormRow type="text" name="location" placeholder="earth" />
-        <FormRow type="email" name="email" placeholder="john@gmail.com" />
-        <FormRow type="password" name="password" placeholder="secret123" />
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
         <SubmitBtn />
         <p>
-          Already a member?{" "}
-          <Link to="/login" className="member-btn">
+          Already a member?
+          <Link to='/login' className='member-btn'>
             Login
           </Link>
         </p>
@@ -47,5 +39,4 @@ const Register = () => {
     </Wrapper>
   );
 };
-
 export default Register;
