e73a8439397a1dacf86e9bb9efdf8059a6a146c0
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 21:02:19 +0430

    clean up

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,39 +5,39 @@ import { UnauthenticatedError } from '../errors/customErrors.js';
 import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
-    const isFirstAccount = (await User.countDocuments()) === 0;
-    req.body.role = isFirstAccount ? 'admin' : 'user';
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
 
-    const hashedPassword = await hashPassword(req.body.password);
-    req.body.password = hashedPassword;
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
 
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ msg: 'user created' });
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 export const login = async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email });
 
-    const isValidUser =
-        user && (await comparePassword(req.body.password, user.password));
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
 
-    if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
+  if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
 
-    const token = createJWT({ userId: user._id, role: user.role });
+  const token = createJWT({ userId: user._id, role: user.role });
 
-    const oneDay = 1000 * 60 * 60 * 24;
+  const oneDay = 1000 * 60 * 60 * 24;
 
-    res.cookie('token', token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + oneDay),
-        secure: process.env.NODE_ENV === 'production',
-    });
-    res.status(StatusCodes.OK).json({ msg: 'user logged in' });
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged in' });
 };
 
 export const logout = (req, res) => {
-    res.cookie('token', 'logout', {
-        httpOnly: true,
-        expires: new Date(Date.now()),
-    });
-    res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
