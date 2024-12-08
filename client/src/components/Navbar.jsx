d3167cd6644eecd776aa7d60cd3db2efae557316
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:50:13 +0430

    correct the spelling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft } from "react-icons/fa";
 import Logo from "./Logo";
-import { userDashboardContext } from "../pages/DashboardLayout";
+import { useDashboardContext } from "../pages/DashboardLayout";
 import LogoutContainer from "./LogoutContainer";
 import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
-  const { toggleSidebar } = userDashboardContext();
+  const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
       <div className="nav-center">
