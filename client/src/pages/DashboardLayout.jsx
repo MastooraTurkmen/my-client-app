bf80a74e7f576d45f0a1dc01867806ae318cf035
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:16 +0430

    set up

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,32 +1,43 @@
-import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom";
-import Wrapper from "../assets/wrappers/Dashboard";
-import { BigSidebar, Navbar, SmallSidebar } from "../components";
-import { createContext, useContext, useState } from "react";
-import customFetch from "../utils/customFetch";
-import { toast } from "react-toastify";
-
-export const loader = async () => {
-  try {
-    const { data } = await customFetch.get("/users/current-user");
+import { Outlet, redirect, useNavigate, useNavigation } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { BigSidebar, Navbar, SmallSidebar, Loading } from '../components';
+import { createContext, useContext, useEffect, useState } from 'react';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+import { useQuery } from '@tanstack/react-query';
+import { checkDefaultTheme } from '../App';
+const userQuery = {
+  queryKey: ['user'],
+  queryFn: async () => {
+    const { data } = await customFetch.get('/users/current-user');
     return data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  try {
+    return await queryClient.ensureQueryData(userQuery);
   } catch (error) {
-    return redirect("/");
+    return redirect('/');
   }
 };
 
 const DashboardContext = createContext();
 
-const DashboardLayout = ({ isDarkThemeEnabled }) => {
-  const { user } = useLoaderData();
+const DashboardLayout = ({ queryClient }) => {
+  const { user } = useQuery(userQuery).data;
   const navigate = useNavigate();
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
+  const [isAuthError, setIsAuthError] = useState(false);
 
   const toggleDarkTheme = () => {
-    const darkTheme = !isDarkTheme;
-    setIsDarkTheme(darkTheme);
-    document.body.classList.toggle("dark-theme", darkTheme);
-    localStorage.setItem("darkTheme", darkTheme);
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
 
   const toggleSidebar = () => {
@@ -34,10 +45,28 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
   };
 
   const logoutUser = async () => {
-    navigate('/')
-    await customFetch.get('/auth/logout')
-    toast.success('Logout successfully')
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    queryClient.invalidateQueries();
+    toast.success('Logging out...');
   };
+
+  customFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error?.response?.status === 401) {
+        setIsAuthError(true);
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    if (!isAuthError) return;
+    logoutUser();
+  }, [isAuthError]);
 
   return (
     <DashboardContext.Provider
@@ -46,18 +75,18 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
         showSidebar,
         isDarkTheme,
         toggleDarkTheme,
-        logoutUser,
         toggleSidebar,
+        logoutUser,
       }}
     >
       <Wrapper>
-        <main className="dashboard">
+        <main className='dashboard'>
           <SmallSidebar />
           <BigSidebar />
           <div>
             <Navbar />
-            <div className="dashboard-page">
-              <Outlet context={{ user }} />
+            <div className='dashboard-page'>
+              {isPageLoading ? <Loading /> : <Outlet context={{ user }} />}
             </div>
           </div>
         </main>
@@ -65,6 +94,5 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
     </DashboardContext.Provider>
   );
 };
-
-export const userDashboardContext = () => useContext(DashboardContext);
+export const useDashboardContext = () => useContext(DashboardContext);
 export default DashboardLayout;
