6fc6248759eb809d4e1448da5cd1ebc15ec17f4f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:46:14 +0430

    clean up

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,24 +1,19 @@
-import React from "react";
-
 const FormRow = ({ type, name, labelText, defaultValue, onChange }) => {
   return (
-    <div>
-      <div className="form-row">
-        <label htmlFor={name} className="form-label">
-          {labelText || name}
-        </label>
-        <input
-          defaultValue={defaultValue || ""}
-          type={type}
-          required
-          id={name}
-          name={name}
-          className="form-input"
-          onChange={onChange}
-        />
-      </div>
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className="form-input"
+        defaultValue={defaultValue || ""}
+        onChange={onChange}
+        required
+      />
     </div>
   );
 };
-
 export default FormRow;
