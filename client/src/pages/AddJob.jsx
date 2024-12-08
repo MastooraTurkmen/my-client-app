d7ea18a9d74e9611adf07e46d2e90072288cf102
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:01 +0430

    correct the spelling

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,10 +1,10 @@
-import { FormRow, FormRowSelect, SubmitBtn } from "../components";
-import Wrapper from "../assets/wrappers/DashboardFormPage";
-import { useOutletContext } from "react-router-dom";
-import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
-import { Form, redirect } from "react-router-dom";
-import { toast } from "react-toastify";
-import customFetch from "../utils/customFetch";
+import { FormRow, FormRowSelect, SubmitBtn } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
 
 export const action =
   (queryClient) =>
@@ -12,10 +12,10 @@ export const action =
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
     try {
-      await customFetch.post("/jobs", data);
-      queryClient.invalidateQueries(["jobs"]);
-      toast.success("Job added successfully ");
-      return redirect("all-jobs");
+      await customFetch.post('/jobs', data);
+      queryClient.invalidateQueries(['jobs']);
+      toast.success('Job added successfully ');
+      return redirect('all-jobs');
     } catch (error) {
       toast.error(error?.response?.data?.msg);
       return error;
@@ -27,26 +27,26 @@ const AddJob = () => {
 
   return (
     <Wrapper>
-      <Form method="post" className="form">
-        <h4 className="form-title">add job</h4>
-        <div className="form-center">
-          <FormRow type="text" name="position" />
-          <FormRow type="text" name="company" />
+      <Form method='post' className='form'>
+        <h4 className='form-title'>add job</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='position' />
+          <FormRow type='text' name='company' />
           <FormRow
-            type="text"
-            labelText="job location"
-            name="jobLocation"
+            type='text'
+            labelText='job location'
+            name='jobLocation'
             defaultValue={user.location}
           />
           <FormRowSelect
-            labelText="job status"
-            name="jobStatus"
+            labelText='job status'
+            name='jobStatus'
             defaultValue={JOB_STATUS.PENDING}
             list={Object.values(JOB_STATUS)}
           />
           <FormRowSelect
-            labelText="job type"
-            name="jobType"
+            labelText='job type'
+            name='jobType'
             defaultValue={JOB_TYPE.FULL_TIME}
             list={Object.values(JOB_TYPE)}
           />
