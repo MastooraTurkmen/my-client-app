a8fdea3b49c550ebe0fe3bee6d865cc83c8b7f0e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 21:05:06 +0430

    clean up

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,11 +1,33 @@
-import { Router } from 'express'
-const router = Router()
-import { getAllJobs, getSingleJob, updateJob, createJob, deleteJob, showStats } from '../controllers/jobController.js'
-import { validateJobInput, validateIdParams } from '../middleware/validationMiddleware.js'
-import { checkForTestUser } from '../middleware/authMiddleware.js'
+import { Router } from 'express';
+const router = Router();
+import {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+  showStats,
+} from '../controllers/jobController.js';
+import {
+  validateJobInput,
+  validateIdParam,
+} from '../middleware/validationMiddleware.js';
+import { checkForTestUser } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getAllJobs).post(checkForTestUser, validateJobInput, createJob)
-router.route('/stats').get(showStats)
-router.route('/:id').get(validateIdParams, getSingleJob).patch(validateJobInput, updateJob).delete(checkForTestUser, validateIdParams, deleteJob)
+// router.get('/',getAllJobs)
+// router.post('/',createJob)
+
+router
+  .route('/')
+  .get(getAllJobs)
+  .post(checkForTestUser, validateJobInput, createJob);
+
+router.route('/stats').get(showStats);
+
+router
+  .route('/:id')
+  .get(validateIdParam, getJob)
+  .patch(checkForTestUser, validateJobInput, validateIdParam, updateJob)
+  .delete(checkForTestUser, validateIdParam, deleteJob);
 
 export default router;
