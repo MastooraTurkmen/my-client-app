26f80f988986f5f7d0250196009190afec2b1245
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-08 20:55:45 +0430

    set up

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,26 +1,32 @@
-import { ChartsContainer, StatsContainer } from "../components";
-import customFetch from "../utils/customFetch";
-import { useLoaderData } from "react-router-dom";
+import { ChartsContainer, StatsContainer } from '../components';
+import customFetch from '../utils/customFetch';
+import { useLoaderData } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
-export const loader = async () => {
-  try {
-    const response = await customFetch.get("/jobs/stats");
+const statsQuery = {
+  queryKey: ['stats'],
+  queryFn: async () => {
+    const response = await customFetch.get('/jobs/stats');
     return response.data;
-  } catch (error) {
-    return error;
-  }
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  const data = await queryClient.ensureQueryData(statsQuery);
+  return null;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplication } = useLoaderData();
+  const { data } = useQuery(statsQuery);
+  const { defaultStats, monthlyApplications } = data;
+
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
-      {monthlyApplication?.length > 1 && (
-        <ChartsContainer data={monthlyApplication} />
+      {monthlyApplications?.length > 1 && (
+        <ChartsContainer data={monthlyApplications} />
       )}
     </>
   );
 };
-
 export default Stats;
