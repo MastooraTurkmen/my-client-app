1af687b4daa46431e86cce5258d942fd78dd0739
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-09 09:36:23 +0430

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 import { ToastContainer } from "react-toastify";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <App />
     <ToastContainer position="top-center" />
-  </StrictMode>
+  </>
 );
